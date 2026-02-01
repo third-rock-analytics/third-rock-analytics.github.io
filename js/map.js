774f8ae25275f50d75611d49f27034dbccef4319
Jeff Zivkovic <jzivkovic1611@gmail.com>
2026-01-31 18:22:31 -0800

    changed basemap

--- js/map.js
+++ js/map.js
@@ -1,12 +1,19 @@
+const key = "PwpQtZrwJrMrtOV5L2aX";
+const styleJson = `https://api.maptiler.com/maps/topo-v4/style.json?key=${key}`;
+
+const attribution = new ol.control.Attribution({
+  collapsible: false,
+});
+
 const map = new ol.Map({
   target: "map",
-  layers: [
-    new ol.layer.Tile({
-      source: new ol.source.OSM(),
-    }),
-  ],
+  controls: ol.control.defaults
+    .defaults({ attribution: false })
+    .extend([attribution]),
   view: new ol.View({
-    center: [0, 0],
-    zoom: 2,
+    constrainResolution: true,
+    center: ol.proj.fromLonLat([0, 0]),
+    zoom: 1,
   }),
 });
+olms.apply(map, styleJson);
