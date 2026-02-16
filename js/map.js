42b1d3882e44048dd8ce55cdac2319654a7fc9fb
Jeff Zivkovic <jzivkovic1611@gmail.com>
2026-02-15 23:08:39 -0800

    working map with bbox

--- js/map.js
+++ js/map.js
@@ -1,19 +1,20 @@
-const key = "PwpQtZrwJrMrtOV5L2aX";
-const styleJson = `https://api.maptiler.com/maps/topo-v4/style.json?key=${key}`;
+const openfreemap = new ol.layer.Group()
 
-const attribution = new ol.control.Attribution({
-  collapsible: false,
+const bbox = ol.proj.transformExtent(
+  [-130, 20, -61.5, 53],
+  'EPSG:4326',
+  'EPSG:3857'
+)
+
+const view = new ol.View({
+  center: ol.proj.fromLonLat([-98.5, 39.8]),
+  zoom: 4,
+  extent: bbox
 });
 
 const map = new ol.Map({
-  target: "map",
-  controls: ol.control.defaults
-    .defaults({ attribution: false })
-    .extend([attribution]),
-  view: new ol.View({
-    constrainResolution: true,
-    center: ol.proj.fromLonLat([0, 0]),
-    zoom: 1,
-  }),
-});
-olms.apply(map, styleJson);
+  layers: [openfreemap],
+  view: view,
+  target: 'map',
+})
+olms.apply(openfreemap, 'https://tiles.openfreemap.org/styles/bright');
