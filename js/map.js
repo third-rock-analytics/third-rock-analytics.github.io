ac4cf5ce02a16129cc8b5b02ead7619408f605d7
Jeff Zivkovic <jzivkovic1611@gmail.com>
2026-02-15 23:27:40 -0800

    add map interaction

--- js/map.js
+++ js/map.js
@@ -1,20 +1,47 @@
-const openfreemap = new ol.layer.Group()
+const openfreemap = new ol.layer.Group();
+
+const container = document.getElementById("popup");
+const content = document.getElementById("popup-content");
+const closer = document.getElementById("popup-closer");
+
+const overlay = new ol.Overlay({
+  element: container,
+  autoPan: {
+    animation: {
+      duration: 250,
+    },
+  },
+});
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
 
 const bbox = ol.proj.transformExtent(
   [-130, 20, -61.5, 53],
-  'EPSG:4326',
-  'EPSG:3857'
-)
+  "EPSG:4326",
+  "EPSG:3857",
+);
 
 const view = new ol.View({
   center: ol.proj.fromLonLat([-98.5, 39.8]),
   zoom: 4,
-  extent: bbox
+  extent: bbox,
 });
 
 const map = new ol.Map({
   layers: [openfreemap],
   view: view,
-  target: 'map',
-})
-olms.apply(openfreemap, 'https://tiles.openfreemap.org/styles/bright');
+  overlays: [overlay],
+  target: "map",
+});
+olms.apply(openfreemap, "https://tiles.openfreemap.org/styles/bright");
+
+map.on("singleclick", function (evt) {
+  const coordinate = evt.coordinate;
+  const hdms = ol.coordinate.toStringHDMS(ol.proj.toLonLat(coordinate));
+
+  content.innerHTML = "<p>You clicked here:</p><code>" + hdms + "</code>";
+  overlay.setPosition(coordinate);
+});
